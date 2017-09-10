c0938bf74006de50935aedaf4670512493a2264d
Song <song@china.world>
2017-09-10 12:39:19 +0800

    For clear

--- evaluation.py
+++ evaluation.py
@@ -7,72 +7,49 @@ class Evaluation:
 
     VERSION = 1.0
 
+    COUPON_SQL = ''' SELECT CouponTable.skuid, CouponTable.specialPrice, CouponTable.link as couponLink,
+                         SkuTable.price, SkuTable.comRate, HistoryTable.list 
+                     FROM `CouponTable` 
+                     INNER JOIN SkuTable ON SkuTable.skuid = CouponTable.skuid
+                     INNER JOIN HistoryTable ON HistoryTable.skuid = CouponTable.skuid'''
+
+    DISCOUNT_SQL = ''' SELECT DiscountTable.skuid, DiscountTable.specialPrice,
+                           SkuTable.price, SkuTable.comRate, HistoryTable.list 
+                       FROM `DiscountTable` 
+                       INNER JOIN SkuTable ON SkuTable.skuid = DiscountTable.skuid
+                       INNER JOIN HistoryTable ON HistoryTable.skuid = DiscountTable.skuid'''
+
+    SECKILL_SQL = ''' SELECT SeckillTable.skuid, SeckillTable.specialPrice,
+                          SeckillTable.startTime, SeckillTable.endTime,
+                          SkuTable.price, SkuTable.comRate, HistoryTable.list
+                      FROM `SeckillTable` 
+                      INNER JOIN SkuTable ON SkuTable.skuid = SeckillTable.skuid
+                      INNER JOIN HistoryTable ON HistoryTable.skuid = SeckillTable.skuid'''
+
     def __init__(self, configFile, db):
 
         self.configFile = configFile
         self.db = db
 
-        self.specialList = list()
-
-    def evaluateCoupon(self):
-
-        sql = ''' SELECT CouponTable.skuid, CouponTable.specialPrice, CouponTable.link as couponLink,
-                      SkuTable.price, SkuTable.comRate, HistoryTable.list 
-                  FROM `CouponTable` 
-                  INNER JOIN SkuTable ON SkuTable.skuid = CouponTable.skuid
-                  INNER JOIN HistoryTable ON HistoryTable.skuid = CouponTable.skuid'''
-
-        result = self.db.query(sql)
-        if result is None:
-            return
-
-        for row in result:
-            special = Special(row, Evaluation.VERSION)
-            special.update()
-            self.specialList.append(special)
-            print special
-
-    def evaluateDiscount(self):
-        sql = ''' SELECT DiscountTable.skuid, DiscountTable.specialPrice,
-                      SkuTable.price, SkuTable.comRate, HistoryTable.list 
-                  FROM `DiscountTable` 
-                  INNER JOIN SkuTable ON SkuTable.skuid = DiscountTable.skuid
-                  INNER JOIN HistoryTable ON HistoryTable.skuid = DiscountTable.skuid'''
-
-        result = self.db.query(sql)
-        if result is None:
-            return
-
-        for row in result:
-            special = Special(row, Evaluation.VERSION)
-            special.update()
-            self.specialList.append(special)
-            print special
-
-    def evaluateSeckill(self):
-        sql = ''' SELECT SeckillTable.skuid, SeckillTable.specialPrice,
-                      SeckillTable.startTime, SeckillTable.endTime,
-                      SkuTable.price, SkuTable.comRate, HistoryTable.list
-                  FROM `SeckillTable` 
-                  INNER JOIN SkuTable ON SkuTable.skuid = SeckillTable.skuid
-                  INNER JOIN HistoryTable ON HistoryTable.skuid = SeckillTable.skuid'''
-
-        result = self.db.query(sql)
-        if result is None:
-            return
-
-        for row in result:
-            special = Special(row, Evaluation.VERSION)
-            special.update()
-            self.specialList.append(special)
-            print special
-
     def evaluate(self):
 
-        self.evaluateCoupon()
-        self.evaluateDiscount()
-        self.evaluateSeckill()
+        self.specialList = list()
 
-    def store(self):
-        pass
+        sqls = [Evaluation.COUPON_SQL, Evaluation.DISCOUNT_SQL, Evaluation.SECKILL_SQL]
+
+        for sql in sqls:
+            result = self.db.query(sql)
+
+            if result is None:
+                continue
+
+            for row in result:
+                special = Special(row, Evaluation.VERSION)
+                special.update()
+
+                self.db.insert('SpecialTable', special.data, ['skuid'])
+
+                self.specialList.append(special)
+
+        print len(self.specialList)
 
