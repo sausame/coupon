b29c42ab1c11e972bf1a2d77112319fbfa384864
sausame <bi_song@163.com>
2017-09-27 11:58:27 +0800

    History table will check for itself

--- evaluation.py
+++ evaluation.py
@@ -55,7 +55,7 @@ class Evaluation:
                   AND skuid NOT IN ( SELECT DiscountTable.skuid FROM DiscountTable )
                   AND skuid NOT IN ( SELECT SeckillTable.skuid FROM SeckillTable )'''
 
-        tableNames = ['SkuTable', 'InformationTable', 'HistoryTable']
+        tableNames = ['SkuTable', 'InformationTable']
 
         for tableName in tableNames:
 
