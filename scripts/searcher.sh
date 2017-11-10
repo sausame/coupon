3abf06007a3729542ab9f770a8bb1739aa13cb0b
sausame <bi_song@163.com>
2017-11-10 14:59:26 +0800

    Use absolute path

--- scripts/searcher.sh
+++ scripts/searcher.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-source command.sh
-
 if [ $# -lt 2 ]; then
     echo -e "usages: \n\t$0 content [savefile]\n"
     exit 1
 fi
 
-APP="searcher.py"
+DIR=$(dirname "$0")
+source "$DIR/command.sh"
+
+APP=$(basename "$0")
+APP=${APP//\.sh/.py}
+
 CONFIG_FILE="/xxx/config.ini"
 
 run $APP $CONFIG_FILE $@
